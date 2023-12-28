a12cbcb1e1a21002305246b05deddc90209d1ab4
IagoraNz <iago.almeida@ufpi.edu.br>
2023-12-28 10:44:30 -0300

    Backup

--- Pré-recesso/Introdução/aula.pl
+++ Pré-recesso/Introdução/aula.pl
@@ -1,9 +1,4 @@
-somalista([], 0).
-somalista([Cabeca|Cauda], Soma) :-
-    somalista(Cauda, SomaCauda),
-    Soma is Cabeca + SomaCauda.
-
-exemplosoma :-
-    Lista = [1, 2, 3, 4, 5],
-    somalista(Lista, Resultado),
-    write('A soma da lista: '), write(Resultado).
+somalista([], 0). %Explicita o caso base, quando a lista é vazia, a soma é 0
+somalista([Cabeca|Cauda], Soma) :- %Caso recursivo, a lista não é vazia, então a soma é a cabeça da lista + a soma da cauda da lista
+    somalista(Cauda, SomaCauda), %Chama recursivamente a função somalista, passando a cauda da lista e a soma da cauda da lista
+    Soma is Cabeca + SomaCauda. %Soma a cabeça da lista com a soma da cauda da lista e atribui o resultado a variável Soma
