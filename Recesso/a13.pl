efb9d6af16916db7b9605fc24a61e17d6fb806e3
IagoraNz <iago.almeida@ufpi.edu.br>
2023-12-29 14:27:22 -0300

    Backup

---
+++ Recesso/a13.pl
@@ -0,0 +1,17 @@
+inicio() :-
+    write('Digite duas listas: '),
+    read(L1),
+    read(L2),
+    
+    comum(X, L1, L2),
+    write('As listas possuem '), write(X), write(' elementos em comum'), nl.
+
+comum(0, [], _).
+comum(0, _, []).
+comum(X, [H|T], L2) :-
+    member(H, L2),
+    comum(X1, T, L2),
+    X is X1 + 1.
+comum(X, [H|T], L2) :-
+    not(member(H, L2)),
+    comum(X, T, L2).
