6c60b594b458c6fc2cdb77234cf3ae526ef13e26
IagoraNz <iago.almeida@ufpi.edu.br>
2024-01-09 09:01:38 -0300

    Backup

---
+++ Recesso/Treino/a37.pl
@@ -0,0 +1,26 @@
+amigo(joao, pedro).
+amigo(carlos, joao).
+amigo(maria, ana).
+amigo(ana, joao).
+amigo(ana, maria).
+amigo(maria, pedro).
+amigo(luiz, maria).
+amigo(luiz, andressa).
+amigo(andresa, carlos).
+
+main() :-
+    write('Digite o nome da pessoa: '),
+    read(Pessoa),
+
+    contaamigos(Pessoa, Total),
+    write('Total de amigos: '), write(Total), nl.
+
+contaamigos(Pessoa, Total) :-
+    contaamigos(Pessoa, 0, Total).
+
+contaamigos(Pessoa, Cont, Total) :-
+    amigo(Pessoa, Amigo),
+    Cont1 is Cont + 1,
+    contaamigos(Amigo, Cont1, Total).
+
+contaamigos(_, Total, Total).
