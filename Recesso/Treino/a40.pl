1b62184e280a6f13b56116056e92d91676eb3ab8
IagoraNz <iago.almeida@ufpi.edu.br>
2024-01-11 11:02:03 -0300

    Backup

--- Recesso/Treino/a40.pl
+++ Recesso/Treino/a40.pl
@@ -10,3 +10,8 @@ adiciona(_, [], []).
 adiciona(Elem, [H|T], [Elem, H|T]).
 adiciona(Elem, [H|T], [H|T1]) :-
     adiciona(Elem, T, T1).
+
+% Outra forma, adicionando o elemento ao final da lista:
+% adiciona(Elem, [], [Elem]).
+% adiciona(Elem, [H|T], [H|T1]) :-
+%     adiciona(Elem, T, T1).
