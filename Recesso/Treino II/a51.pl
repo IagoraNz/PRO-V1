7d56accc477e0b9d3a454949129ea0226f1a06a0
IagoraNz <iago.almeida@ufpi.edu.br>
2024-01-14 19:56:53 -0300

    Backup

--- Recesso/Treino II/a51.pl
+++ Recesso/Treino II/a51.pl
@@ -1,9 +1,49 @@
-mamifero(leao).
-mamifero(elefante).
-carnivoro(leao).
-herbivoro(elefante).
-carnivoro(cobra).
+invertebrado(pulga).
+invertebrado(lagarta).
+invertebrado(sanguessuga).
+invertebrado(minhoca).
 
-predador(X) :-
-    mamifero(X),
-    carnivoro(X).
+vertebrado(aguia).
+vertebrado(pomba).
+vertebrado(homem).
+vertebrado(vaca).
+
+ave(aguia).
+ave(pomba).
+
+inseto(pulsa).
+inseto(lagarta).
+
+anelideo(sanguessuga).
+anelideo(minhoca).
+
+mamifero(homem).
+mamifero(vaca).
+
+carnivoro(aguia).
+
+onivoro(pomba).
+onivoro(homem).
+onivoro(minhoca).
+
+herbivoro(vaca).
+herbivoro(lagarta).
+
+hematofago(pulga).
+hematofago(sanguessuga).
+
+main :-
+    (
+        vertebrado(X),
+        (
+            (ave(X), (carnivoro(X) ; onivoro(X)), write(X), nl, fail);
+            (mamifero(X), (onivoro(X) ; herbivoro(X)), write(X), nl, fail)
+        )
+    );
+    (
+        invertebrado(X),
+        (
+            (inseto(X), (hematofago(X) ; herbivoro(X)), write(X), nl, fail);
+            (anelideo(X), (hematofago(X) ; onivoro(X)), write(X), nl, fail)
+        )
+    ).
