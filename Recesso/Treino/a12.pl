fc6f282e271648c54183e5b808ac92016e861faa
IagoraNz <iago.almeida@ufpi.edu.br>
2024-01-15 16:44:04 -0300

    Backup

--- Recesso/Treino/a12.pl
+++ Recesso/Treino/a12.pl
@@ -11,4 +11,5 @@ main() :-
 listasomada([], _, []).
 listasomada([H|T], Num, [HS|TS]) :-
     listasomada(T, Num, TS),
-    HS is H + Num.
+    HS is H + Num. %A cabeça da lista somada com o numero é a cabeça da lista somada
+    %Somando, portanto, com todos os elementos da lista.
--- Recesso/Treino/a13.pl
+++ Recesso/Treino/a13.pl
@@ -6,13 +6,17 @@ inicio() :-
     comum(X, L1, L2),
     write('As listas possuem '), write(X), write(' elementos em comum'), nl.
 
-comum(0, [], _).
-comum(0, _, []).
-comum(X, [H|T], L2) :-
-    member(H, L2),
-    comum(X1, T, L2),
+comum(0, [], _). % Caso base, se a lista 1 for vazia, não há elementos em comum
+comum(0, _, []). % Caso base, se a lista 2 for vazia, não há elementos em comum
+comum(X, [H|T], L2) :- 
+    membro(H, L2), % Se o elemento H estiver na lista 2
+    comum(X1, T, L2), % Verifica se os elementos restantes da lista 1 estão na lista 2
     X is X1 + 1.
 
 comum(X, [H|T], L2) :-
     not(member(H, L2)),
     comum(X, T, L2).
+
+membro(X, [X|_]).
+membro(X, [_|T]) :- 
+    membro(X, T).
